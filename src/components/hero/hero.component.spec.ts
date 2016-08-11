13b71a4496a312a022e60584b67d280780265441
Quinntyne Brown <quinntynebrown@gmail.com>
2016-08-11 18:56:31 -0400

    testing

--- src/components/hero/hero.component.spec.ts
+++ src/components/hero/hero.component.spec.ts
@@ -1,1 +1,5 @@
-﻿
+﻿import { async, inject } from '@angular/core/testing';
+import {  } from '@angular/compiler/testing';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
